30d85587d5b2ad1e3f043be8b5d1c26f44dc8f9c
GaslenkoDI <GaslenkoDI@loodsen.ru>
2023-03-20 13:23:33 +0300

    1

--- HW02/HW02.sql
+++ HW02/HW02.sql
@@ -1,12 +1,9 @@
 /*
 Домашнее задание по курсу MS SQL Server Developer в OTUS.
 Занятие "02 - Оператор SELECT и простые фильтры, JOIN".
-
 Задания выполняются с использованием базы данных WideWorldImporters.
-
 Бэкап БД WideWorldImporters можно скачать отсюда:
 https://github.com/Microsoft/sql-server-samples/releases/download/wide-world-importers-v1.0/WideWorldImporters-Full.bak
-
 Описание WideWorldImporters от Microsoft:
 * https://docs.microsoft.com/ru-ru/sql/samples/wide-world-importers-what-is
 * https://docs.microsoft.com/ru-ru/sql/samples/wide-world-importers-oltp-database-catalog
@@ -24,20 +21,13 @@ USE WideWorldImporters
 Таблицы: Warehouse.StockItems.
 */
 
- 
-	use[WideWorldImporters]
-
 SELECT 
-		 StockItemID,
-		 StockItemName,
-		 UnitPrice,
-		 RecommendedRetailPrice
-	FROM Warehouse.StockItems
-	WHERE
-		StockItemName LIKE '%urgent%'
-		OR StockItemName LIKE 'Animal%'
-
-	GO
+StockItemID, StockItemName
+FROM 
+Warehouse.StockItems
+WHERE
+StockItemName LIKE '%urgent%' OR
+StockItemName LIKE 'Animal%'
 
 /*
 2. Поставщиков (Suppliers), у которых не было сделано ни одного заказа (PurchaseOrders).
@@ -47,16 +37,12 @@ SELECT
 По каким колонкам делать JOIN подумайте самостоятельно.
 */
 
+SELECT s.SupplierID, s.SupplierName
+FROM Purchasing.Suppliers s
+LEFT JOIN Purchasing.PurchaseOrders po ON po.SupplierID=s.SupplierID
+WHERE po.PurchaseOrderID IS NULL
+ORDER BY s.SupplierID
 
-	use[WideWorldImporters]
-SELECT 
-		 ps.SupplierID,
-		 ps.SupplierName
-	FROM 
-		Purchasing.Suppliers as ps
-		LEFT JOIN Purchasing.PurchaseOrders as ppo ON ps.SupplierID = ppo.SupplierID
-	WHERE
-		ppo.PurchaseOrderID IS NULL
 
 /*
 3. Заказы (Orders) с ценой товара (UnitPrice) более 100$ 
@@ -71,28 +57,49 @@ SELECT
 * имя заказчика (Customer)
 Добавьте вариант этого запроса с постраничной выборкой,
 пропустив первую 1000 и отобразив следующие 100 записей.
-
 Сортировка должна быть по номеру квартала, трети года, дате заказа (везде по возрастанию).
-
 Таблицы: Sales.Orders, Sales.OrderLines, Sales.Customers.
 */
+-- 1 вариант
+SELECT   o.OrderID,  
+         ol.OrderLineId,
+         FORMAT(o.OrderDate, 'dd.MM.yyyy') as OrderDate,
+		 DATENAME (month,o.OrderDate) as Month_name, 
+		 DATEPART(quarter, o.OrderDate) as QuarterN,
+         case when datepart(month, o.OrderDate) between 1 and 4 then 1 
+              when datepart(month, o.OrderDate) between 5 and 8 then 2
+              when datepart(month, o.OrderDate) between 9 and 12 then 3 end as ThirdYear, 
+         c.CustomerName
 
-SELECT o.OrderID, CONVERT(NVARCHAR, o.OrderDate, 104) AS OrderDate, c.CustomerName, DATENAME(MM, o.OrderDate) AS Mounth, DATEPART(QUARTER, o.OrderDate) AS [Quarter],
-CASE WHEN MONTH(o.OrderDate) <5 THEN 1 WHEN MONTH(o.OrderDate) > 8 THEN 3 ELSE 2 END [Quarter1]
-FROM Sales.Orders AS o
-JOIN Sales.OrderLines AS ol ON o.OrderID = ol.OrderID 
-JOIN Sales.Customers AS c ON o.CustomerID = c.CustomerID
-WHERE ol.UnitPrice > 100 or ol.Quantity > 20 and ol.PickingCompletedWhen is not null
-ORDER BY [Quarter], [Quarter1], o.OrderDate
+FROM Sales.Orders o
+JOIN Sales.OrderLines ol ON o.OrderID=ol.OrderID
+JOIN Sales.Customers c ON o.CustomerID=c.CustomerID
+WHERE (ol.UnitPrice>100 OR ol.Quantity>20 ) AND (o.PickingCompletedWhen is not NULL)
+ORDER BY QuarterN, ThirdYear, o.OrderDate
 
-SELECT o.OrderID, CONVERT(NVARCHAR, o.OrderDate, 104) AS OrderDate, c.CustomerName, DATENAME(MM, o.OrderDate) AS Mounth, DATEPART(QUARTER, o.OrderDate) AS [Quarter],
-CASE WHEN MONTH(o.OrderDate) <5 THEN 1 WHEN MONTH(o.OrderDate) > 8 THEN 3 ELSE 2 END [Quarter1]
-FROM Sales.Orders AS o
-JOIN Sales.OrderLines AS ol ON o.OrderID = ol.OrderID 
-JOIN Sales.Customers AS c ON o.CustomerID = c.CustomerID
-WHERE ol.UnitPrice > 100 or ol.Quantity > 20 and ol.PickingCompletedWhen is not null
-ORDER BY [Quarter], [Quarter1], o.OrderDate OFFSET 1000 rows fetch first 100 rows only
 
+-- 2 вариант
+-- постраничный вывод
+DECLARE 
+	@pagesize BIGINT = 100, -- Размер страницы
+	@pagenum  BIGINT = 1000;  -- Номер страницы
+
+SELECT  o.OrderID,  
+         ol.OrderLineId,
+         FORMAT(o.OrderDate, 'dd.MM.yyyy') as OrderDate,
+		 DATENAME (month,o.OrderDate) as Month_name, 
+		 DATEPART(quarter, o.OrderDate) as QuarterN,
+         case when datepart(month, o.OrderDate) between 1 and 4 then 1 
+              when datepart(month, o.OrderDate) between 5 and 8 then 2
+              when datepart(month, o.OrderDate) between 9 and 12 then 3 end as ThirdYear, 
+         c.CustomerName
+
+FROM Sales.Orders o
+JOIN Sales.OrderLines ol ON o.OrderID=ol.OrderID
+JOIN Sales.Customers c ON o.CustomerID=c.CustomerID
+WHERE (ol.UnitPrice>100 OR ol.Quantity>20 ) AND (o.PickingCompletedWhen is not NULL)
+ORDER BY QuarterN, ThirdYear, o.OrderDate
+OFFSET @pagenum ROWS FETCH FIRST @pagesize ROWS ONLY; 
 
 /*
 4. Заказы поставщикам (Purchasing.Suppliers),
@@ -104,28 +111,22 @@ ORDER BY [Quarter], [Quarter1], o.OrderDate OFFSET 1000 rows fetch first 100 row
 * дата доставки (ExpectedDeliveryDate)
 * имя поставщика
 * имя контактного лица принимавшего заказ (ContactPerson)
-
 Таблицы: Purchasing.Suppliers, Purchasing.PurchaseOrders, Application.DeliveryMethods, Application.People.
 */
 
-	
-SELECT
-	ppo.PurchaseOrderID,
-	ppo.OrderDate,
-	ppo.ExpectedDeliveryDate,
-	adm.DeliveryMethodName,
-	ps.SupplierName,
-	ap.FullName AS [Contact Person Name]
-	FROM Purchasing.PurchaseOrders AS ppo
-	JOIN Application.DeliveryMethods as adm ON ppo.DeliveryMethodID = adm.DeliveryMethodID
-	LEFT JOIN Purchasing.Suppliers as ps ON ppo.SupplierID = ps.SupplierID
-	LEFT JOIN Application.People as ap ON ppo.ContactPersonID = ap.PersonID
-	WHERE
-	ppo.ExpectedDeliveryDate BETWEEN '20130101' AND '20130131'
-	AND ppo.IsOrderFinalized = 1
-	AND adm.DeliveryMethodName IN ('Air Freight', 'Refrigerated Air Freight')
-
-	GO
+SELECT 
+	dm.DeliveryMethodName,
+	po.ExpectedDeliveryDate,
+	s.SupplierName,
+	p.FullName
+FROM Purchasing.Suppliers s
+	JOIN Purchasing.PurchaseOrders po ON po.SupplierID=s.SupplierID
+	JOIN Application.DeliveryMethods dm ON dm.DeliveryMethodID=po.DeliveryMethodID
+	JOIN Application.People p ON p.PersonID=po.ContactPersonID
+WHERE
+	(po.ExpectedDeliveryDate between '20130101' and '20130131') and
+	(dm.DeliveryMethodName IN ('Air Freight','Refrigerated Air Freight')) and
+	po.IsOrderFinalized=1
 
 /*
 5. Десять последних продаж (по дате продажи) с именем клиента и именем сотрудника,
@@ -133,18 +134,16 @@ SELECT
 Сделать без подзапросов.
 */
 
-SELECT TOP 10
-		 so.OrderID,
-		 so.OrderDate,
-		 sc.CustomerName,
-		 ap.FullName
-	FROM Sales.Orders AS so
-		LEFT JOIN Sales.Customers AS sc ON so.CustomerID = sc.CustomerID
-		LEFT JOIN Application.People AS ap ON so.SalespersonPersonID = ap.PersonID
-	ORDER BY
-		so.OrderID DESC
-
-	GO
+SELECT TOP 10 
+	o.OrderID, 
+	o.OrderDate, 
+	o.CustomerID, 
+	cap.FullName AS Employee
+	.CustomerName,
+FROM Sales.Orders o
+	JOIN Sales.Customers c ON c.CustomerID=o.CustomerID
+	JOIN Application.People ap on ap.PersonID=o.SalespersonPersonID
+	order by o.OrderDate desc
 
 /*
 6. Все ид и имена клиентов и их контактные телефоны,
@@ -152,19 +151,23 @@ SELECT TOP 10
 Имя товара смотреть в таблице Warehouse.StockItems.
 */
 
-	DECLARE @StockItemName nvarchar(100) = 'Chocolate frogs 250g', @StockItemID int;
-
-	SET @StockItemID = (SELECT StockItemID FROM Warehouse.StockItems WHERE StockItemName = @StockItemName)
-
-	SELECT DISTINCT
-		 sc.CustomerID,
-		 sc.CustomerName,
-		 sc.PhoneNumber
-	FROM Warehouse.StockItems as wsi
-		 JOIN Sales.OrderLines as sol ON wsi.StockItemID = sol.StockItemID
-		 JOIN Sales.Orders as so ON sol.OrderID = so.OrderID
-		 JOIN Sales.Customers as sc ON sc.CustomerID = so.CustomerID
-	WHERE
-		wsi.StockItemID = @StockItemID
-
-	GO
+SELECT 
+	c.CustomerID,
+	c.CustomerName,
+	count(ol.OrderID) AS ORDERS_COUNT,
+	c.PhoneNumber,
+	c.FaxNumber
+FROM 
+	Warehouse.StockItems si
+	JOIN Sales.OrderLines ol ON ol.StockItemID=si.StockItemID
+	JOIN Sales.Orders o ON o.OrderID=ol.OrderID
+	JOIN Sales.Customers c ON c.CustomerID=o.CustomerID
+WHERE
+	si.StockItemName='Chocolate frogs 250g'
+GROUP BY
+	c.CustomerID,
+	c.CustomerName,
+	c.PhoneNumber,
+	c.FaxNumber
+ORDER BY
+	c.CustomerName
